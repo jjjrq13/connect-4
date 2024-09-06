0fa3c6187748816aabfa8e8dbec789798c55fe04
JonathanRQ <jonathan.ruizquintero@gmail.com>
2024-09-06 02:25:03 -0400

    Gmae is wokring, damn display is pissing me off

--- js/app.js
+++ js/app.js
@@ -11,31 +11,54 @@ console.log(allButtons);
 const nameInputs = document.querySelectorAll('input');
 console.log(nameInputs);
 
-const board = document.querySelector('#board');
-console.log(board)
+const domBoard = [
+    document.querySelectorAll('.column-A'),
+    document.querySelectorAll('.column-B'),
+    document.querySelectorAll('.column-C'),
+    document.querySelectorAll('.column-D'),
+    document.querySelectorAll('.column-E'),
+    document.querySelectorAll('.column-F'),
+    document.querySelectorAll('.column-G'),
+];
+
+const gameBoard = {
+    A: [0, 0, 0, 0, 0, 0],
+    B: [0, 0, 0, 0, 0, 0],
+    C: [0, 0, 0, 0, 0, 0],
+    D: [0, 0, 0, 0, 0, 0],
+    E: [0, 0, 0, 0, 0, 0],
+    F: [0, 0, 0, 0, 0, 0],
+    G: [0, 0, 0, 0, 0, 0],
+};
 
 //------------------------ LET ------------------------
 
 let playerOneName;
 let playerTwoName;
+let currentPlayer;
 
 //------------------------ FUNCTIONS ------------------------
 
-
-//display message function 
+//display message function
 const displayMessage = (element) => {
-    document.querySelector('#display-message').textContent = `${element}`;;
-}
+    document.querySelector('#display-message').textContent = `${element}`;
+};
 
 //player name function
 const playerNameTrue = () => {
     if (nameInputs[0].value && nameInputs[1].value) {
-        document.querySelector('#player-red-name').textContent = `${nameInputs[0].value}`;
-        playerOneName = `${nameInputs[0].value}`
+        document.querySelector(
+            '#player-red-name',
+        ).textContent = `${nameInputs[0].value}`;
+        playerOneName = `${nameInputs[0].value}`;
 
-        document.querySelector('#player-yellow-name').textContent = `${nameInputs[1].value}`;
-        playerTwoName = `${nameInputs[1].value}`
-        console.log(`Player Red: ${playerOneName}, Player Yellow: ${playerTwoName}`);
+        document.querySelector(
+            '#player-yellow-name',
+        ).textContent = `${nameInputs[1].value}`;
+        playerTwoName = `${nameInputs[1].value}`;
+        console.log(
+            `Player Red: ${playerOneName}, Player Yellow: ${playerTwoName}`,
+        );
         return true;
     } else {
         nameInputs.forEach((name) => {
@@ -72,11 +95,62 @@ const beginGame = () => {
         allSections[2].classList.remove('hidden');
         allSections[1].classList.add('hidden');
     }
+    whoWillStartIsTheQuestion();
 };
 
 const playAgainButton = () => {
-    console.log('Gmae Has Restarted');
+    console.log('game Has Restarted');
+};
+
+const whoWillStartIsTheQuestion = () => {
+    let random = Math.round(Math.random());
+    if (playerOneName && playerTwoName) {
+        let start = [playerOneName, playerTwoName];
+        currentPlayer = start[random];
+    }
+    console.log(currentPlayer);
+    displayMessage(`${currentPlayer} you will start the game!`);
+};
+
+//game fucntions
+
+const next = () => {
+    if(currentPlayer === playerOneName){
+        currentPlayer = playerTwoName;
+    } else {
+        currentPlayer = playerOneName;
+    }
 }
+
+
+const assignPiece = (event) => {
+    let column = event.target.id[0];
+    console.log(column);
+    let row = gameBoard[column].indexOf(0);
+    let img = document.createElement('img');
+    let id = document.querySelector(`#${column}${row}`);
+
+    if (currentPlayer === playerOneName) {
+        gameBoard[column][row] = 'R';
+        img.setAttribute('src', './assets/red-piece.svg');
+        img.setAttribute('alt', 'red playing piece');
+        img.setAttribute('id', `${column}${row}-img`);
+        id.appendChild(img);
+        id.style = 'background-color: transparent';
+        next();
+    } else {
+        gameBoard[column][row] = 'Y';
+        img.setAttribute('src', './assets/yellow-piece.svg');
+        img.setAttribute('alt', 'yellow playing piece');
+        img.setAttribute('id', `${column}${row}-img`);
+        id.appendChild(img);
+        id.style = 'background-color: transparent';
+        next();
+    }
+};
+
+
+
 
 //------------------------ EVENT LISTNERS ------------------------
 
@@ -97,6 +171,14 @@ allButtons.forEach((btn) => {
     }
 });
 
+domBoard.forEach((column) => {
+    column.forEach((row) => {
+        row.addEventListener('click', assignPiece);
+    });
+});
+
+
+//------------------------ GAME CODE ------------------------
+
 //game code
 
-displayMessage('You can suck it ')
